a2e63dbd2aaf7294ee371f2762e33bf2ba510083
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-23 13:05:22 +0100

    tryting to work out test three

--- src/friends.py
+++ src/friends.py
@@ -6,3 +6,7 @@ def get_name(person):
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
 
+def likes_to_eat(person, food_item):
+    for snack in person:
+        if person["favourites"]["snacks"] == food_item:
+            return True
