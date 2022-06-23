9681ead0ef5e551f0206efc997306bf64b4f974a
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-06-23 13:13:34 +0100

    test three done

--- src/friends.py
+++ src/friends.py
@@ -1,5 +1,8 @@
 
 
+from xml.etree.ElementPath import prepare_descendant
+
+
 def get_name(person):
     return person["name"]
 
@@ -7,6 +10,9 @@ def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
 
 def likes_to_eat(person, food_item):
-    for snack in person:
-        if person["favourites"]["snacks"] == food_item:
-            return True
+    snacks = person["favourites"]["snacks"]
+    food_item_there = False
+    for snack in snacks:
+        if snack == food_item:
+            food_item_there = True   
+    return food_item_there
